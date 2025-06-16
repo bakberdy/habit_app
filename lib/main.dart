f5dcfccb85c0e14a25bdfa5b663bf2bccc6a115d
Bakberdi <bakberdy.ye@gmail.com>
2025-06-16 11:21:44 +0500

    switch of .env

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: '.env');
+  // await dotenv.load(fileName: '.env');
   configureDependencies();
   Bloc.observer = sl<BlocObserver>();
   runApp(const MyApp());
